86358ac4d7170828f6745a3af2a610528a515936
mohraihanromadhoni <107664700+mohraihanromadhoni@users.noreply.github.com>
2022-06-17 10:57:08 +0700

    Add files via upload

---
+++ cTraansaksi.java
@@ -0,0 +1,39 @@
+
+package pkgprojectUAS;
+
+
+public class cTraansaksi {
+    String id;
+    String kode;
+    String pembeli;
+    cItem barang;
+    int jmlbrg;
+    int status;
+    cTraansaksi next;
+    cTraansaksi(String k,String p,cItem b,int jml,int s){
+        kode=k; pembeli=p; barang=b; jmlbrg=jml; status=s;
+        next=null;
+    }
+    public void setStatus(int s){
+        s=status;
+    }
+    public String getKode(){
+        return kode;
+    }
+    public String getpembeli(){
+        return pembeli;
+    }
+    public cItem getItem(){
+        return barang;
+    }
+    public int getjumlahbrg(){
+        return jmlbrg;
+    }
+    public int getStatus(){
+        return status;
+    }
+    public String getID(){
+        return id;
+    }
+    
+}
